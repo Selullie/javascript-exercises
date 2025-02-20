72c6b5dced10ed35ba6347004aa9d7935de51c04
Selullie <142448746+Selullie@users.noreply.github.com>
2025-02-20 16:03:31 +0000

    Completed 05

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,24 @@
-const sumAll = function() {
+const sumAll = function(first, second) {
+    if (!Number.isInteger(first) || !Number.isInteger(second)) return "ERROR";
+    if(first < 0 || second < 0) return "ERROR";
+    if (first > second) {
+        const temp = first;
+        first = second;
+        second = temp;
+    }
 
+    let sum = 0;
+    for (i = first; i <= second; i++){
+        sum += i;
+    }
+    return sum;
 };
+
+/*
+receive 2 numbers
+find all numbers between 1st and 2nd number
+
+*/
 
 // Do not edit below this line
 module.exports = sumAll;
