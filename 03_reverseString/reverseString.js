b107e00c98cda91f3f5cf64d2ce3e41c8c5ffe87
Selullie <142448746+Selullie@users.noreply.github.com>
2025-02-20 14:25:58 +0000

    Completed 03

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,11 @@
-const reverseString = function() {
-
+const reverseString = function(word) {
+    let string = "";
+    let i = word.length;
+    while (i >= 0){
+        string += word.charAt(i);
+        i--;
+    }
+    return string;
 };
 
 // Do not edit below this line
