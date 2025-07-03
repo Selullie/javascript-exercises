56b9231816ce55fdeaf5155a3fee5b931d871df6
Selullie <142448746+Selullie@users.noreply.github.com>
2025-07-03 15:59:10 +0100

    Exercise 12 complete

--- 12_calculator/calculator.js
+++ 12_calculator/calculator.js
@@ -1,25 +1,49 @@
-const add = function() {
-	
+const add = function(num1, num2) {
+  let sum = 0;
+  sum = num1 + num2;
+  return sum;	
 };
 
-const subtract = function() {
-	
+const subtract = function(num1, num2) {
+	let sum = 0;
+  sum = num1 - num2;
+  return sum;
 };
 
-const sum = function() {
-	
+const sum = function(numArray) {
+	let sum = 0;
+  for (let i = 0; i < numArray.length; i++) {
+    sum = sum + numArray[i];
+  }
+  return sum;
 };
 
-const multiply = function() {
-
+const multiply = function(numArray) {
+  let sum = 1;
+  for (let i = 0; i < numArray.length; i++) {
+    sum *= numArray[i];
+  }
+  return sum;
 };
 
-const power = function() {
-	
+const power = function(num1, num2) {
+  let sum = 1;
+  let numArray = [];
+  for (let i = 0; i < num2; i++) {
+    numArray[i] = num1;    
+  }
+  for (let i = 0; i < numArray.length; i++) {
+    sum *= numArray[i];    
+  }
+  return sum;	
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+  if (num === 0 || num === 1 ) {
+    return 1;
+  } else {
+    return num * factorial(num - 1);
+  }
 };
 
 // Do not edit below this line
